494cae439a79ce6a4f164db33e3c1c18b8eba553
Laura Santos <alsr16p@gmail.com>
2021-12-04 17:17:29 +0000

    added card component, bootstrap, fontawesome for icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CribListingComponent
+    CribListingComponent,
+    CribCardComponent
   ],
   imports: [
     BrowserModule,
